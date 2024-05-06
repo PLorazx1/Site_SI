124691b273af0ab513bd8efed539d1a46fdcd515
Paulo Loraschi <161669867+PLorazx1@users.noreply.github.com>
2024-05-06 14:47:33 -0300

    teste final 3.1

---
+++ Cliente/Comandos.js
@@ -0,0 +1,15 @@
+function Entrar(){
+    window.location.href = "index.html";
+}
+
+function Codigo(){
+    window.location.href = "codigo.html";
+}
+
+function Nova_senha(){
+    window.location.href = "novaSenha.html";
+}
+
+function Login(){
+    window.location.href = "login.html"
+}
